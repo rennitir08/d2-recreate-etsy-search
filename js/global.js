a1ce40d12b233b1f0dcbf4654b934cc6455f23af
YOUR NAMEJennifer <YOUR EMAIL ADDRESSjenniferbonner12@gmail.com>
2017-03-01 21:14:47 -0500

    added ajax

--- js/global.js
+++ js/global.js
@@ -1,91 +1,51 @@
 // Variables
-
+var info = {};
 // Logic
+getEtsyData();
+
 createCard({
     src: 'http://unsplash.it/400/300?image=500',
-    caption:'Image',
-    author:'Author',
+    caption:'Images',
+    author:'Shop',
     price:'Price',
 });
-createCard({
-    src: 'http://unsplash.it/400/300?image=600',
-    caption:'Image',
-    author:'Author',
-    price:'Price',
-});
-createCard({
-    src: 'http://unsplash.it/400/300?image=700',
-    caption:'Image',
-    author:'Author',
-    price:'Price',
-});
-createCard({
-    src: 'http://unsplash.it/400/300?image=800',
-    caption:'Image',
-    author:'Author',
-    price:'Price',
-});
-createCard({
-    src: 'http://unsplash.it/400/300?image=900',
-    caption:'Image',
-    author:'Author',
-    price:'Price',
-});
-createCard({
-    src: 'http://unsplash.it/400/300?image=110',
-    caption:'Image',
-    author:'Author',
-    price:'Price',
-});
-createCard({
-    src: 'http://unsplash.it/400/300?image=120',
-    caption:'Image',
-    author:'Author',
-    price:'Price',
-});
-createCard({
-    src: 'http://unsplash.it/400/300?image=130',
-    caption:'Image',
-    author:'Author',
-    price:'Price',
-});
-createCard({
-    src: 'http://unsplash.it/400/300?image=140',
-    caption:'Image',
-    author:'Author',
-    price:'Price',
-});
-createCard({
-    src: 'http://unsplash.it/400/300?image=300',
-    caption:'Image',
-    author:'Author',
-    price:'Price',
-});
-createCard({
-    src: 'http://unsplash.it/400/300?image=160',
-    caption:'Image',
-    author:'Author',
-    price:'Price',
-});
-createCard({
-    src: 'http://unsplash.it/400/300?image=170',
-    caption:'Image',
-    author:'Author',
-    price:'Price',
-});
+// cre
 // functions
+
+function getEtsyData() {
+    fetch('https://thinksaydo.com/tiyproxy.php?https://openapi.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=' + encodeURIComponent('board games') + '&includes=Images,Shop')
+        .then(response => response.json())
+        .then(data => {
+           info = data;
+           renderCreateCard();
+           console.log(info);
+        });
+}
+
+function renderCreateCard() {
+    info.results.forEach(createCard);
+}
+
+
 function createCard(image) {
     var card = `<div class="card col-sm-4">
-        <img src="${image.src}" alt="Image"/>
-        <div class="card-caption">${image.caption}</div>
+        <img src="${image.Images[0].url_170x135}" alt="Image"/>
+        <div class="card-caption">${image.title.slice(0,50)}...</div>
         <div class="row">
         <div class="col-sm-6">
-        <div class="card-author text-muted">${image.author}</div>
+        <div class="card-author text-muted">${image.Shop.shop_name}</div>
         </div>
         <div class="col-sm-6">
-        <div class="card-price">${image.price}</div>
+        <div class="card-price">$${image.price}</div>
             </div>
         </div>
     </div>`;
     document.querySelector('.app').innerHTML += card;
+}
+
+function keyPress() {
+  getEtsyData();
+}
+function enterSearch() {
+      document.querySelector('#search').addEventListener('click', keypress);
 }
