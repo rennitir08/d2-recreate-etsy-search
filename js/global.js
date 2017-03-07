ef99e68c37807770a3783f51421f003e9107f18f
YOUR NAMEJennifer <YOUR EMAIL ADDRESSjenniferbonner12@gmail.com>
2017-03-07 13:49:46 -0500

    corrected search field

--- js/global.js
+++ js/global.js
@@ -1,51 +1,70 @@
 // Variables
-var info = {};
+var info 
+var searchTerm = 'hats'
 // Logic
 getEtsyData();
 
-createCard({
-    src: 'http://unsplash.it/400/300?image=500',
-    caption:'Images',
-    author:'Shop',
-    price:'Price',
-});
-// cre
+    document.querySelector('#searchTerm').addEventListener('keypress', function(e) {
+        if(e.key === 'Enter') {
+            searchEtsy();
+        }
+    });
+
+document.querySelector('#searchButton').addEventListener('click', searchEtsy);
+
 // functions
 
-function getEtsyData() {
-    fetch('https://thinksaydo.com/tiyproxy.php?https://openapi.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=' + encodeURIComponent('board games') + '&includes=Images,Shop')
+function searchEtsy() {
+    searchTerm = document.querySelector('#searchTerm').value;
+    if (searchTerm !== '') {
+        fetch('https://thinksaydo.com/tiyproxy.php?https://openapi.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=' + encodeURIComponent(searchTerm) + '&includes=Images,Shop')
         .then(response => response.json())
-        .then(data => {
-           info = data;
-           renderCreateCard();
-           console.log(info);
-        });
+        .then(loopThroughSearchResults);
+    }
 }
 
-function renderCreateCard() {
-    info.results.forEach(createCard);
+function loopThroughSearchResults(data) {
+
+    document.querySelector('.app').innerHTML = '';
+    if (data.results.length > 0) {
+            createCard(data.results);
+    }
 }
 
+function createCard(items) {
+    console.log(items)
+    items.forEach(function(details){
 
-function createCard(image) {
     var card = `<div class="card col-sm-4">
-        <img src="${image.Images[0].url_170x135}" alt="Image"/>
-        <div class="card-caption">${image.title.slice(0,50)}...</div>
+        <img src="${details.Images[0].url_170x135}" alt="Image"/>
+        <div class="card-caption">${details.title.slice(0,50)}...</div>
         <div class="row">
         <div class="col-sm-6">
-        <div class="card-author text-muted">${image.Shop.shop_name}</div>
+        <div class="card-author text-muted">${details.Shop.shop_name}</div>
         </div>
         <div class="col-sm-6">
-        <div class="card-price">$${image.price}</div>
+        <div class="card-price">$${details.price}</div>
             </div>
         </div>
     </div>`;
+
     document.querySelector('.app').innerHTML += card;
+        })
 }
 
-function keyPress() {
-  getEtsyData();
+function getEtsyData() {
+    console.log(searchTerm)
+
+    fetch('https://thinksaydo.com/tiyproxy.php?https://openapi.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=' + encodeURIComponent(searchTerm) + '&includes=Images,Shop')
+        .then(response => response.json())
+        .then(data => {
+           info = data.results;
+           createCard(info);
+        });
 }
-function enterSearch() {
-      document.querySelector('#search').addEventListener('click', keypress);
-}
+
+
+
+
+
+
